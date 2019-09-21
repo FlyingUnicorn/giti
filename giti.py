3d3462752c0a2042da758b78ffa680c6234abf2e
Jonas Andersson <jonas8andersson@gmail.com>
2019-09-21 21:10:14 +0200

    box boarder
    Change-Id: I6428082f5767f61455c74c006ac9a0f550d2a2cf

--- giti.py
+++ giti.py
@@ -150,7 +150,7 @@ def get_valid_entry(str_filter, major, entry):
     return (h, major, date, time, cr, an, ae, s, b)
 
 def get_formatted_entry(display, str_filter, columns, user_name, user_email, entry, major_toggle):
-    str_out_sep = ' | '
+    str_out_sep = ' \U00002502 '
 
     (h, m, d, t, cr, an, ae, s, _) = entry
     dct_fields = {
@@ -295,10 +295,9 @@ def getChar():
 
 def format_select(display, entry):
 
-    columns = display.columns
+    columns = display.columns - 2
     (h, m, d, t, cr, an, ae, s, b) = entry
 
-    display.add_select_delimiter()
     strselect = ''
     strselect_l = 'Author:  {} ({})\n'.format(an, ae)
     strselect_l += 'Date:    {} {} ({})\n'.format(d, t, cr)
@@ -366,7 +365,7 @@ def format_select(display, entry):
             else:
                 strselect += lst_l[n]
                 printed_l = len(lst_l[n])
-        strselect += ' ' * (split_column - printed_l) + ' | '
+        strselect += ' ' * (split_column - printed_l) + ' \U00002502 '
 
         if n < lst_r_len:
             if n == max_rows:
@@ -415,44 +414,57 @@ class Display:
         elif displaypos == DisplayPos.SELECT:
             self.screenbuffer_select.append(underline)
 
-    def add_select_delimiter(self):
-        underline = self.palette.underline
-        self.screenbuffer_select.append(underline)
-        self.screenbuffer_select.append(underline + '\U00002588' * self.columns)
+    #def add_select_delimiter(self):
+    #    self.screenbuffer_select.append()
+    #    self.screenbuffer_select.append(underline + '\U00002588' * self.columns)
 
     def color_text(self, palettecolor, text):
         return self.palette.d[palettecolor] + text + self.palette.d[PaletteColors.TEXT]
 
     def max_log_lines(self):
-        return self.rows - len(self.screenbuffer_info) - len(self.screenbuffer_select)
+        len_select = 0
+        if len(self.screenbuffer_select) > 0:
+           len_select = len(self.screenbuffer_select) + 1
+        return self.rows - len(self.screenbuffer_info) - len_select - 3
 
     def draw(self, selected_indx, select_active):
-        
+
         bg = self.palette.d[PaletteColors.BACKGROUND]
         fg = self.palette.d[PaletteColors.TEXT]
         select_bg = self.palette.d[PaletteColors.SELECT_BG] if select_active else self.palette.d[PaletteColors.SELECT_INACTIVE_BG]
         select_fg = self.palette.d[PaletteColors.SELECT_FG] if select_active else self.palette.d[PaletteColors.SELECT_INACTIVE_FG]
         reset = self.palette.reset
 
-        strbuf = ''
+
+        strbuf = bg + fg + '\U0000250F' + '\U00002501' * (self.columns - 2) + '\U00002513' + reset
         for ln in self.screenbuffer_info:
-            strbuf += bg + fg + pad(ln, self.columns, '<') + reset
+            strbuf += bg + fg + '\U00002503' + pad(ln, self.columns - 2, '<') + '\U00002503' + reset
+
+        strbuf += bg + fg + '\U00002520' + '\U00002500' * (self.columns - 2) + '\U00002528' + reset
 
         for indx, ln in enumerate(self.screenbuffer_log):
-            ln = bg + fg + pad(ln, self.columns, '<') + reset
             if indx == selected_indx:
-                ln = ln.replace(fg, select_fg)
+                ln = pad(ln, self.columns - 2, '<')
                 ln = ln.replace(bg, select_bg)
+                ln = ln.replace(fg, select_fg)
+                ln = bg + fg + '\U00002503' + reset + select_bg + select_fg + ln + reset + bg + fg + '\U00002503' + reset
+            else:
+                ln = bg + fg + '\U00002503' + pad(ln, self.columns - 2, '<') + '\U00002503' + reset
             strbuf += ln
 
-        pad_lines = self.rows - len(self.screenbuffer_info) - len(self.screenbuffer_log) - len(self.screenbuffer_select)
+        strbuf_select = ''
+        if self.screenbuffer_select:
+            strbuf_select += bg + fg + '\U00002523' + '\U00002501' * (self.columns - 2) + '\U0000252B' + reset
+        for ln in self.screenbuffer_select:
+            strbuf_select += bg + fg + '\U00002503' + pad(ln, self.columns - 2, '<') + '\U00002503' + reset
+
+        pad_lines = self.rows - len(self.screenbuffer_info) - len(self.screenbuffer_log) - len(strbuf_select) - 3
         if pad_lines > 0:
             for _ in range(pad_lines):
-                strbuf += '\n' + bg + ' ' * self.columns + reset
+                strbuf += '\n' + bg + fg + '\U00002503' + ' ' * (self.columns - 2) + '\U00002503' + reset
 
-        for ln in self.screenbuffer_select:
-            strbuf += bg + fg + pad(ln, self.columns, '<') + reset
-
+        strbuf += strbuf_select
+        strbuf += bg + fg + '\U00002517' + '\U00002501' * (self.columns - 2) + '\U0000251B' + reset
         display.clear()
         sys.stdout.writelines(strbuf)
         sys.stdout.flush()
@@ -460,12 +472,37 @@ class Display:
         self.screenbuffer_log = []
         self.screenbuffer_select = []
 
+    def short(self, entries):
+        bg = self.palette.d[PaletteColors.BACKGROUND]
+        fg = self.palette.d[PaletteColors.TEXT]
+        reset = self.palette.reset
+
+        strbuf = '\U0000250F' + '\U00002501' * (self.columns - 2) + '\U00002513' + reset
+        for ln in self.screenbuffer_log[:entries]:
+            strbuf += fg + '\U00002503' + pad(ln, self.columns - 2, '<') + '\U00002503' + reset
+
+        strbuf += '\n' + '\U00002517' + '\U00002501' * (self.columns - 2) + '\U0000251B' + reset
+
+        sys.stdout.writelines(strbuf)
+        sys.stdout.flush()
+
 
 if __name__ == '__main__':
 
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
     theme = 'themes/dark.txt'
+
+    short = False
+    if len(sys.argv) >= 3 and sys.argv[1] == '--short':
+        entries = int(sys.argv[2], 10)
+        args = sys.argv[3:]
+        theme = 'themes/dark.txt'
+        short = True
+    else:
+        entries = 10000
+        args = sys.argv[1:]
+
     palette = Palette(theme)
     display = Display(palette)
     #for n in range(1, 255):
@@ -488,7 +525,7 @@ if __name__ == '__main__':
     result = subprocess.run(cmd_gitemail, stdout=subprocess.PIPE, shell=True)
     user_email = result.stdout.decode('utf-8').strip()
 
-    lst_sorted = gitlog(sys.argv[1:], 10000)
+    lst_sorted = gitlog(args, 50 + entries)
     current = 0
     last_current = - 2
     str_filter = ''
@@ -503,7 +540,6 @@ if __name__ == '__main__':
     last_xselect_entry = False
     xexec = False
     last_xexec = False
-    str_exec = ''
     last_str_exec = ''
     lst_matchesx = []
     rows = rows - 2
@@ -515,10 +551,10 @@ if __name__ == '__main__':
     consecutive_str = ''
     consecutive_input = False
 
+    str_exec = ''
+
     action = 'up'
     while (True):
-
-
         if total_log_entries == None: total_log_entries = len(lst_matchesx) # first time only
 
         if action or consecutive_input:
@@ -630,7 +666,7 @@ if __name__ == '__main__':
 
             infobar_l = display.color_text(PaletteColors.TEXT_ALT1, 'GIT') + display.color_text(PaletteColors.TEXT_ALT2, 'i') + ' ' + str_cmd_filter
             infobar_r = '{} ({})'.format(user_name, user_email)
-            infobar = pad(infobar_l, columns - len(infobar_r), '<') + infobar_r
+            infobar = pad(infobar_l, columns - 2 - len(infobar_r), '<') + infobar_r
             display.add_line(DisplayPos.INFO, infobar)
 
             if str_filter or xfilter:
@@ -641,12 +677,10 @@ if __name__ == '__main__':
                 execbar = 'Execute command on commit ({}): {}'.format(exec_hash, str_exec)
                 display.add_line(DisplayPos.INFO, execbar)
 
-            str_debug = 'xspos: {} current: {} matches: {}'.format(xselect_pos, current, len(lst_matchesx))
+            str_debug += 'xspos: {} current: {} matches: {}'.format(xselect_pos, current, len(lst_matchesx))
             if str_debug:
                 debugbar = 'DEBUG: {}'.format(str_debug)
-                display.add_line(DisplayPos.INFO, debugbar)
-
-            display.add_underline(DisplayPos.INFO)
+                #display.add_line(DisplayPos.INFO, debugbar)
 
             if xselect_entry:
                 format_select(display, lst_matchesx[current + xselect_pos])
@@ -685,6 +719,17 @@ if __name__ == '__main__':
                 xselect_diff = False
                 subprocess.call('git show {}'.format(h), shell=True)
 
+            if action == 'commit-rebase':
+                (h, _, _, _, _, _, _, _, _) = lst_matchesx[current + xselect_pos]
+                #xselect_diff = False
+                display.clear()
+                editor = os.environ['EDITOR']
+                os.environ['EDITOR'] = 'vi'
+                subprocess.call('git rebase -i {}^'.format(h), shell=True)
+                os.environ['EDITOR'] = editor
+                sys.exit()
+
+
             major_toggle = False
             last_major = ''
             for indx, matchx in enumerate(lst_matchesx[current:current + max_log_lines]):
@@ -695,11 +740,15 @@ if __name__ == '__main__':
                 if m != last_major:
                     major_toggle = not major_toggle
                     last_major = m
-                formatted_entry = get_formatted_entry(display, str_filter, columns - 6, user_name, user_email, matchx, major_toggle)
+                formatted_entry = get_formatted_entry(display, str_filter, columns - 8, user_name, user_email, matchx, major_toggle)
                 logentry = '{:4}: {}'.format(current+indx, formatted_entry)
                 display.add_line(DisplayPos.LOG, logentry)
 
-            display.draw(xselect_pos, xselect)
+            if short:
+                display.short(entries)
+                sys.exit()
+            else:
+                display.draw(xselect_pos, xselect)
             #last_xfilter = xfilter
             #last_xselect = xselect
             #last_xselect_pos = xselect_pos
@@ -710,18 +759,17 @@ if __name__ == '__main__':
 
 
         meta = False
-
+        str_exec = ''
         action = ''
         ch = getChar()
         ich = ord(ch[0])
-        str_debug = ''
 
         if ich == 27:
             meta = True
             ch = getChar()
             ich = ord(ch[0])
 
-        #str_debug += '{}/{} {}'.format(ch, ich, meta)
+        str_debug += '{}/{} {}'.format(ch, ich, meta)
         #print(str_debug)
 
         if ich == 27: # esc
@@ -745,11 +793,14 @@ if __name__ == '__main__':
         elif ch == ' ':
             action = 'commit-select'
 
-        elif ch == 'x':
+        elif ch == '`':
             action = 'commit-execute-command'
 
         elif ch == 'i':
             action = 'commit-show-info'
+
+        elif ch == 'r':
+            action = 'commit-rebase'
 
         elif ch == 'p' or ich == 183: # p or arrow
             action = 'up'
