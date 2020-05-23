b6a0dffbeedcf02efefe43a1e999e3ae726bb6a9
Jonas Andersson <jonas8andersson@gmail.com>
2020-05-24 01:54:26 +0200

    fixed config

--- giti.c
+++ giti.c
@@ -2124,7 +2124,7 @@ main()
 
     giti_window_opt_t opt = giti_summary_create(current_branch, user_name, user_email);
     giti_window_stack_t* gws = giti_window_stack_create(opt);
-#if 1
+#if 0
     //https://htmlcolorcodes.com/color-chart/
     giti_color_scheme_t cs = {
         .fg          = (giti_color_t){ .c = 0xD1F2EB },
