3bae919fab69759ed6ee8bee08c5ed04ad9f1f2c
Jonas Andersson <jonas8andersson@gmail.com>
2020-05-24 14:43:43 +0200

    set op

--- giti_config.c
+++ giti_config.c
@@ -26,16 +26,43 @@
 #define DEFAULT_DOWN    'j'
 
 
+giti_config_t config;
+
+enum {
+    SET,
+    APPEND,
+};
+
+#define CONFIG \
+  X(STR_TIMEOUT, SET, DEFAULT_TIMEOUT, config.timeout)         \
+  X(STR_UP,      SET, DEFAULT_UP,      config.navigation.up)   \
+  X(STR_DOWN,    SET, DEFAULT_DOWN,    config.navigation.down) \
+
+#define FMT(T) _Generic( (T), \
+    char:  "%c",              \
+    int:   "%d",              \
+    long:  "%ld",             \
+    char*: "%s"               \
+)
 
 
-#define FMT(T) _Generic( (T), char: "%c", int: "%d", long: "%ld", char*: "%s", default: "")
+long format_long(const char* str) { return atol(str); }
+char format_char(const char* str) { return *str; }
+
+
+#define format(T, str) _Generic( (T), \
+    char: format_char(str),           \
+    long: format_long(str)            \
+)
+
+#if 0
 static void
 print()
 {
 
   log("--------------------------");
 
-  int x = 0;
+  int x = 100;
   (void)x;
 
   const char* t =FMT(x);
@@ -44,6 +71,15 @@ print()
 
   log(t, x);
 
+  char buffer[128];
+#define X(str, def, ptr)                                   \
+    ptr = def;                                             \
+    log(str);                                              \
+    snprintf(buffer, sizeof(buffer), "%s: %s", str, FMT(ptr)); \
+    log(buffer, ptr);
+
+    CONFIG
+#undef X
 
 
 
@@ -58,7 +94,7 @@ print()
 
     log("--------------------------");
 }
-
+#endif
 
 
 static char*
@@ -90,13 +126,15 @@ DEFAULT_FRIENDS);
     return len > 0 ? str_config : NULL;
 }
 
-const char*
+char*
 giti_config_get_value(const char* str) {
 
-   char* str_value = strchr(str, ':');
-
+  char* str_value = strchr(str, ':');
    str_value += 1;
 
+   log("[%s] str: <%s> val: <%s>", __func__, str_value, strtrimall(str_value));
+
+#if 0
    while (!isprint(*str_value)) {
        if (*str_value == '\n') {
            str_value = NULL;
@@ -106,17 +144,91 @@ giti_config_get_value(const char* str) {
            str_value += 1;
        }
    }
-
  exit:
+#endif
+
    return strtrimall(str_value);
 }
 
+
+
+void
+giti_config_line(const char* line)
+{
+  //log("[%s] config line: %s", __func__, line);
+
+  char value[128] = { 0 };
+  size_t value_pos = 0;
+  for (const char* ch = strchr(line, ':'); *ch; ++ch) {
+    if (isspace(*ch) || *ch == ':') {
+      continue;
+    }
+    value[value_pos++] = *ch;
+  }
+
+  if (!value_pos) {
+    return;
+  }
+
+#define X(str, op, def, ptr)                     \
+    if (strncmp(str, line, strlen(str)) == 0) {  \
+      if (op == SET) {                           \
+          ptr = format(ptr, value);              \
+      }                                          \
+    }
+
+    CONFIG
+#undef X
+}
+
+giti_config_t*
+giti_config_create(char* str_config)
+{
+   config.friends = dlist_create(); // todo fix this
+  //log("%s\n", str_config);
+    str_config = str_config ? str_config : giti_config_default_create();
+    char* curline = str_config;
+    while(curline) {
+        char* nextLine = strchr(curline, '\n');
+        if (nextLine) {
+            *nextLine = '\0';
+            nextLine += 1;
+        }
+
+        if (*curline == '#' || isspace(*curline)) {
+            goto next;
+        }
+
+        giti_config_line(curline);
+
+
+#if 0
+        if (strncmp(curline, STR_TIMEOUT, strlen(STR_TIMEOUT)) == 0) {
+            const char* str_value = giti_config_get_value(curline);
+            if (str_value) {
+                config->timeout = atol(str_value);
+            }
+#endif
+next:
+        curline = nextLine;
+    }
+
+    log("?????????????????");
+    giti_config_print(&config);
+    log("?????????????????");
+
+
+    return &config;
+}
+
+#if 0
 giti_config_t*
 giti_config_create(char* str_config)
 {
     print();
 
     str_config = str_config ? str_config : giti_config_default_create();
+    giti_config_create2(str_config);
 
     log("config:\n%s", str_config);
 
@@ -188,6 +300,7 @@ next:
 
     return config;
 }
+#endif
 
 void
 giti_config_print(const giti_config_t* config)
